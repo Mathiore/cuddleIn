70b5852f031ea0cf0ccd81b5a89bf1afd058ee78
Mathiore <mraimundo@edu.univali.br>
2021-08-07 19:33:29 -0300

    fixing navbar

--- template/js/main.js
+++ template/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     $('.fa-bars').click(function () {
         $(this).toggleClass('fa-hand-holding-heart');
-        $('nav').toggleClass('nav-toggle');
+        $('#topmenu').toggleClass('nav-toggle');
     });
     $('nav ul li a').click(function () {
         $('.fa-bars').removeClass('fa-hand-holding-heart');
-        $('nav').removeClass('nav-toggle');
+        $('#topmenu').removeClass('nav-toggle');
     });
 });
