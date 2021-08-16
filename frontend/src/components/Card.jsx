638ea30131356b0f7c25bdf0bd653790053041e2
Mathiore <mraimundo@edu.univali.br>
2021-08-15 23:31:10 -0300

    Applying modal and changing images

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,5 +2,16 @@ import React from 'react';
 
 export default Card =>
     <div>
-        
+        <div class="card">
+                <a href="product.html"><img class="medium" src="img/pe.png" alt="product"></img></a>
+                <div class="card-body">
+                    <a href="product.html"><h2>Massagem nos pés</h2></a>
+                </div>
+                <div class="desc">
+                    <p>Compre uma massagem para os pé para relaxar.</p>
+                </div>
+                <div class="price">
+                    R$0,50
+                </div>
+            </div>
     </div>
